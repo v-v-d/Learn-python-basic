91be8bfe29a165e7d7ef4cdcde3caf00c9d15e31
v-v-d <v-v-d@bk.ru>
2019-07-28 03:40:25 +0300

    hw05_normal mistakes are fixed

--- hw05_normal.py
+++ hw05_normal.py
@@ -16,76 +16,73 @@ import hw05_easy as my_scripts
 import os
 
 
-def return_back(base_dir):
-    if os.getcwd() != base_dir:
-        dir_path = os.path.split(os.getcwd())
-        os.chdir(dir_path[0])
-        return f"\nУспешный переход в папку {dir_path[0]}"
-    else:
-        return "\nВы находитесь в корневой директории. Назад перейти невозможно"
+def return_to_back_dir():
+    dir_path = os.path.split(os.getcwd())
+    os.chdir(dir_path[0])
+    return dir_path[0]
 
 
-def change_dir(base_dir, dir_name):
-    dir_path = os.path.join(base_dir, dir_name)
+def change_dir():
+    if os.getcwd() != BASE_DIR:
+        question_for_back = input("Вернуться в предыдущую папку? [Y/N] ")
+        if question_for_back.lower() == "y":
+            return return_to_back_dir()
+    dir_name = input("Введите имя папки, в которую хотите перейти: ")
+    dir_path = os.path.join(BASE_DIR, dir_name)
     try:
         os.chdir(dir_path)
-        return f"\nУспешный переход в папку {dir_name}\nТекущая директория: {os.getcwd()}"
-    except FileNotFoundError:
-        return f'\nНевозможно перейти в папку {dir_name}. Такой папки не существует.'
+        return dir_path
+    except FileNotFoundError as e:
+        return f'{e}'
 
 
-def console_menu():
+def list_dir():
+    return os.listdir(os.getcwd())
+
+
+def print_menu(menu):
+    print('Консольное меню: ')
+    [print(f'{num}. {key}') for num, key in enumerate(menu, start=1)]
+    print('----------------')
+
+
+def get_command(limit):
+    command = int(input('Выберите действие: '))
+    return command if 1 <= command <= limit else None
+
+
+def my_exit():
+    exit(0)
+
+
+def run_console_menu():
     while True:
-        print("""
-Консольное меню
-
-1. Перейти в папку
-2. Просмотр указанной папки
-3. Удалить папку
-4. Создать папку
-5. Выйти из консольного меню
-        """)
-
+        print_menu(menu)
         try:
-            do = int(input("Выберите действие: "))
-        except ValueError:
-            print('Введите число')
-            continue
-
-        if do == 1:
-            print(my_scripts.list_dirs(os.getcwd()))
-            question_for_back = input("\nВернуться в предыдущую папку? [Y/N] ")
-            question_for_back.lower()
-            if question_for_back == "y":
-                print(return_back(BASE_DIR))
+            command = get_command(len(menu))
+            if command:
+                key = list(menu.keys())[command - 1]
+                if command in (3, 4):
+                    dir_name = input('Введите имя папки: ')
+                    print(menu[key](dir_name))
+                else:
+                    print(menu[key]())
             else:
-                dir_name = input("\nВведите имя папки, в которую хотите перейти: ")
-                print(change_dir(os.getcwd(), dir_name))
-
-        elif do == 2:
-            print(', '.join(my_scripts.list_dirs(os.getcwd())))
-            print(', '.join(my_scripts.list_files(os.getcwd())))
-
-        elif do == 3:
-            print(my_scripts.list_dirs(os.getcwd()))
-            dir_name = input("\nВведите имя папки, которую хотите удалить: ")
-            dir_path = os.path.join(os.getcwd(), dir_name)
-            print(my_scripts.delete_dir(dir_path))
-
-        elif do == 4:
-            dir_name = input("\nВведите имя папки, которую хотите создать: ")
-            print(my_scripts.make_dir(dir_name))
-
-        elif do == 5:
-            print("Успешный выход из консольного меню.")
-            break
-
-        else:
-            if do != range(1, 5):
-                print("Действие не найдено. Попробуйте еще раз.")
+                print('Ошибка! Такого действия нет')
+        except ValueError:
+            print('Ошибка! Введите число')
 
 
-BASE_DIR = os.getcwd()
+menu = {
+    'Перейти в папку': change_dir,
+    'Просмотр указанной папки': list_dir,
+    'Удалить папку': my_scripts.delete_dir,
+    'Создать папку': my_scripts.make_dir,
+    'Выйти из консольного меню': my_exit,
+}
+
 
 if __name__ == '__main__':
-    console_menu()
+    BASE_DIR = os.getcwd()
+
+    run_console_menu()
